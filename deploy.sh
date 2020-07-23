09729edc8d43f995e840145d1d3b4a6fcc35fd83
Sagar Mehta <sagarhere4u@gmail.com>
2020-07-23 12:59:53 +0000

    In

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export TODO_SETTINGS=/home/todo/todo_local.cfg
+export FLASK_APP=todo.py
+
+cd /home/devops/todo
+cv=`git ls-remote --heads https://github.com/dimagi/todo.git master | awk '{print $1}'`
+cvc=`cat /home/devops/todo_with_shell/version | tail -1`
+
+if [[ "$cv" == "$cvc" ]]
+then
+   echo "Application already up to date!"
+else
+   git ls-remote --heads https://github.com/dimagi/todo.git master | awk '{print $1}' >> /home/devops/todo_with_shell/version
+   git fetch
+   git checkout master
+   git pull
+   . env/bin/activate
+   sleep 60
+   pip install -U -r requirements.txt
+   flask db upgrade
+   pkill -u devops flask
+   flask run -h 0.0.0.0 > /dev/null & 
+fi
---
+++ rollback.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export TODO_SETTINGS=/home/todo/todo_local.cfg
+export FLASK_APP=todo.py
+
+cd /home/devops/todo
+var="$1"
+cv=`cat /home/devops/todo_with_shell/version | tail -"$var" | head -1`
+echo $cv
+git reset --hard $cv
+echo "$cv" >> /home/devops/todo_with_shell/version
+git pull
+. env/bin/activate
+sleep 60
+pip install -U -r requirements.txt
+flask db upgrade
+pkill -u devops flask
+flask run -h 0.0.0.0 > /dev/null & 
